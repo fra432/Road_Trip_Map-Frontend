180b465999761abee6e46a3624cd3f5c2da2a817
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-28 13:16:01 +0200

    installed leaflet, added MapContainer to App(test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MapContainer
+        center={[41.346176, 2.168365]}
+        zoom={12}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
+      );
+    </>
+  );
 }
 
 export default App;
