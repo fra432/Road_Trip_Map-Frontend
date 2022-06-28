0afdc00bdda8918f86cbac6252d80ed20586f2fc
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-28 17:08:10 +0200

    added Marker and Popup to MapContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
+import locations from "./data/locations";
+import { Icon } from "leaflet";
 
 function App() {
+  const marker = new Icon({
+    iconUrl: "/location.png",
+    iconSize: [40, 40],
+  });
+
   return (
     <>
       <MapContainer
@@ -14,6 +20,30 @@ function App() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        {locations.features.map((location) => {
+          return (
+            <Marker
+              key={location.properties.LOCATION_ID}
+              position={[
+                location.geometry.coordinates[0],
+                location.geometry.coordinates[1],
+              ]}
+              icon={marker}
+            >
+              <Popup
+                position={[
+                  location.geometry.coordinates[0],
+                  location.geometry.coordinates[1],
+                ]}
+              >
+                <div>
+                  <h2>{location.properties.NAME}</h2>
+                  <p>{location.properties.DESCRIPTIO}</p>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
       );
     </>
---
+++ src/data/locations.ts
@@ -0,0 +1,32 @@
+const locations = {
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        LOCATION_ID: 960,
+        NAME: "El Gato de Botlero",
+        DESCRIPTIO:
+          "Enorme scultura bronzea di un gatto dall'espressione bizzarra del noto artista Fernando Botero",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [41.3787636, 2.1690454],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        LOCATION_ID: 1219,
+        NAME: "MACBA",
+        DESCRIPTIO:
+          "Il Museo di Arte Contemporanea di Barcellona, conosciuto anche con la sigla MACBA, è dedicato fondamentalmente all'esposizione di opere realizzate durante la seconda metà del XX secolo. È situato nel quartiere de El Raval nella città di Barcellona",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [41.3831913, 2.1668668],
+      },
+    },
+  ],
+};
+
+export default locations;
