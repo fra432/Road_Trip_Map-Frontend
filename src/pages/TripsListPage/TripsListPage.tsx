89e47e2c0973edabfedefb32b01b24e41a04a811
Francesco Fabrissin <francescofabrissin@MacBook-Pro.home>
2022-09-01 12:14:37 +0200

    added feedback for user in case there are no trips to be listed

--- src/pages/TripsListPage/TripsListPage.tsx
+++ src/pages/TripsListPage/TripsListPage.tsx
@@ -1,15 +1,29 @@
 import { useEffect } from "react";
 import TripsList from "../../components/TripsList/TripsList";
+import { useAppSelector } from "../../redux/store/hooks";
 import TripsListPageStyled from "./TripsListPageStyled";
 
 const TripsListPage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const { userTrips } = useAppSelector((state) => state.userTrips);
+
   return (
     <TripsListPageStyled>
       <h2 className="title">My Trips</h2>
-      <TripsList />
+      {userTrips.length === 0 ? (
+        <>
+          <p>You haven't created any trip yet.</p>
+          <div className="redirect">
+            <p>Move back to the</p>
+            <a href="/home">Home Page</a>
+            <p>and start planning a new jurney!</p>
+          </div>
+        </>
+      ) : (
+        <TripsList />
+      )}
     </TripsListPageStyled>
   );
 };
